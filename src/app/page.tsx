59fd8d16e04a04898e0f1813508c2d59085a84e5
“Riccardo <silvicardo@gmail.com>
2024-03-20 17:41:28 +0100

    authorization check in home page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,16 @@
+import { unstable_noStore as noStore } from "next/cache";
+
+import { getServerAuthSession } from "~/server/auth";
+
 export default async function Home() {
+  noStore();
+  const session = await getServerAuthSession();
 
   return (
-    <main >
-      Demo
+    <main className="flex min-h-screen flex-col items-center justify-start gap-5 p-5 pt-20">
+      <div className="text-xl">
+        {session?.user === undefined ? "Login required" : "AuthorizedContent"}
+      </div>
     </main>
   );
 }
-
