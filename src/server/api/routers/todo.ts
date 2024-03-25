45e40f41180ba1ff41d25476ea34d51829ca9e00
“Riccardo <silvicardo@gmail.com>
2024-03-25 11:57:12 +0100

    create mutation

--- src/server/api/routers/todo.ts
+++ src/server/api/routers/todo.ts
@@ -1,4 +1,5 @@
 import { createTRPCRouter, protectedProcedure } from "~/server/api/trpc";
+import { z } from "zod";
 
 export const todoRouter = createTRPCRouter({
   getMyTodos: protectedProcedure.query(({ ctx }) =>
@@ -9,4 +10,14 @@ export const todoRouter = createTRPCRouter({
       orderBy: { createdAt: "desc" },
     }),
   ),
+  create: protectedProcedure
+    .input(z.object({ text: z.string().min(1) }))
+    .mutation(({ ctx, input }) =>
+      ctx.db.todo.create({
+        data: {
+          createdBy: { connect: { id: ctx.session.user.id } },
+          text: input.text,
+        },
+      }),
+    ),
 });
